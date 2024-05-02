d94e16a71b9bd352f94efeb4e6d9e4645caaf61e
Robert Nascimento <robert1a7x@gmail.com>
2024-05-02 11:37:31 -0300

    fix/202404/sla_policy fix test

--- enterprise/app/policies/sla_policy_policy.rb
+++ enterprise/app/policies/sla_policy_policy.rb
@@ -1,21 +1,21 @@
-# class SlaPolicyPolicy < ApplicationPolicy
-#   def index?
-#     @account_user.administrator? || @account_user.agent?
-#   end
+class SlaPolicyPolicy < ApplicationPolicy
+  def index?
+    @account_user.administrator? || @account_user.agent?
+  end
 
-#   def update?
-#     @account_user.administrator?
-#   end
+  def update?
+    @account_user.administrator?
+  end
 
-#   def show?
-#     @account_user.administrator? || @account_user.agent?
-#   end
+  def show?
+    @account_user.administrator? || @account_user.agent?
+  end
 
-#   def create?
-#     @account_user.administrator?
-#   end
+  def create?
+    @account_user.administrator?
+  end
 
-#   def destroy?
-#     @account_user.administrator?
-#   end
-# end
+  def destroy?
+    @account_user.administrator?
+  end
+end
